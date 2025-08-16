0ef6a560eb0a7abdff9ffef1d211ab30fb90a398
arthurd3 <arthur_camposl@yahoo.com>
2025-08-16 17:13:46 -0300

    feat: update contact

--- Aue.Stage.Register/Aue.Stage.Register/Forms/MainForm.cs
+++ Aue.Stage.Register/Aue.Stage.Register/Forms/MainForm.cs
@@ -47,7 +47,69 @@ namespace Aue.Stage.Register
 
         private void countContactByCityButton_Click(object sender, EventArgs e)
         {
-           
+            var todosContatos = listAllFromContact.ListAllContact();
+
+            reportListView.Items.Clear();
+            reportListView.Groups.Clear();
+
+            if (todosContatos == null || !todosContatos.Any())
+            {
+                reportListView.Items.Add("Nenhum contato cadastrado para análise.");
+                return;
+            }
+
+
+            int totalHomens = todosContatos.Count(c => c.Sex == "M");
+            int totalMulheres = todosContatos.Count(c => c.Sex == "F");
+
+            ListViewItem totalItem = new ListViewItem("Número de contatos no banco de dados");
+            totalItem.SubItems.Add(todosContatos.Count.ToString());
+            totalItem.SubItems.Add(totalHomens.ToString());
+            totalItem.SubItems.Add(totalMulheres.ToString());
+            totalItem.Font = new Font(reportListView.Font, FontStyle.Bold); 
+            reportListView.Items.Add(totalItem);
+
+            reportListView.Items.Add("");
+
+            var gruposPorCidade = todosContatos.GroupBy(contato => contato.City);
+
+            foreach (var grupoCidade in gruposPorCidade)
+            {
+                string nomeCidade = grupoCidade.Key.ToUpper();
+                var contatosDaCidade = grupoCidade.ToList();
+
+                ListViewGroup cityGroup = new ListViewGroup(nomeCidade, HorizontalAlignment.Left);
+                reportListView.Groups.Add(cityGroup);
+
+                var gruposPorMes = contatosDaCidade
+                    .GroupBy(contato => contato.CreatedAt.Month)
+                    .OrderBy(grupo => grupo.Key); 
+
+                foreach (var grupoMes in gruposPorMes)
+                {
+                    int numeroMes = grupoMes.Key;
+                    string nomeMes = new System.Globalization.CultureInfo("pt-BR").DateTimeFormat.GetMonthName(numeroMes);
+                    nomeMes = char.ToUpper(nomeMes[0]) + nomeMes.Substring(1); 
+
+                    int homensNoMes = grupoMes.Count(c => c.Sex == "M");
+                    int mulheresNoMes = grupoMes.Count(c => c.Sex == "F");
+
+                    ListViewItem monthItem = new ListViewItem($"  . {nomeMes}"); 
+                    monthItem.SubItems.Add(grupoMes.Count().ToString());
+                    monthItem.SubItems.Add(homensNoMes.ToString());
+                    monthItem.SubItems.Add(mulheresNoMes.ToString());
+                    monthItem.Group = cityGroup; 
+                    reportListView.Items.Add(monthItem);
+                }
+
+                ListViewItem cityTotalItem = new ListViewItem($"  . Total");
+                cityTotalItem.SubItems.Add(contatosDaCidade.Count.ToString());
+                cityTotalItem.SubItems.Add(contatosDaCidade.Count(c => c.Sex == "M").ToString());
+                cityTotalItem.SubItems.Add(contatosDaCidade.Count(c => c.Sex == "F").ToString());
+                cityTotalItem.Font = new Font(reportListView.Font, FontStyle.Bold);
+                cityTotalItem.Group = cityGroup;
+                reportListView.Items.Add(cityTotalItem);
+            }
         }
 
         private void includeButton_Click(object sender, EventArgs e)
